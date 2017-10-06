18eaea7bac5b5aa09c0a6e102a7eb6db3e8122f2
Sandytrp <Sandytrp@gmail.com>
2017-10-06 16:03:13 +0530

    2

--- src/test/java/Demo/Insomnia1/AppTest.java
+++ src/test/java/Demo/Insomnia1/AppTest.java
@@ -1,5 +1,8 @@
 package Demo.Insomnia1;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -13,12 +16,17 @@ public class AppTest extends TestCase
     @org.testng.annotations.Test
     public void Testsuite()
     {
-        System.out.println("Line 16");
+      System.setProperty("webdriver.chrome.driver", "Jars\\chromedriver.exe");
+      ChromeDriver driver= new ChromeDriver();
+      driver.get("http://www.Gmail.com");
+      
     }
 
     @org.testng.annotations.Test
     public void testApp()
     {
-    	  System.out.println("Line 22");
+    	System.setProperty("webdriver.gecko.driver", "Jars\\geckodriver.exe");
+    	FirefoxDriver driver= new FirefoxDriver();
+        driver.get("http://www.Gmail.com");
     }
 }
