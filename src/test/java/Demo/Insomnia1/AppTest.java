55b2177d3ad1500de9564591949237c0cbd43dfe
Sandytrp <Sandytrp@gmail.com>
2017-10-06 14:42:37 +0530

    1

---
+++ src/test/java/Demo/Insomnia1/AppTest.java
@@ -0,0 +1,24 @@
+package Demo.Insomnia1;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    
+    @org.testng.annotations.Test
+    public void Testsuite()
+    {
+        System.out.println("Line 16");
+    }
+
+    @org.testng.annotations.Test
+    public void testApp()
+    {
+    	  System.out.println("Line 22");
+    }
+}
